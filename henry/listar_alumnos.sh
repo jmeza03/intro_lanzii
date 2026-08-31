5d3f2d75a7ab0d85a793cde4d271778748f639f4
luishenry145-coder <luishenry145@gmail.com>
2026-08-31 18:20:30 -0300

    Create listar_alumnos.sh to display sorted students
    Add script to list students sorted by ID from a file.

--- henry/listar_alumnos.sh
+++ henry/listar_alumnos.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+archivo=~/$CREACION/salida/$FILENAME.txt
+
+if [ -f "$archivo" ]; then
+    echo "Mostrando alumnos ordenados por número de padrón:"
+    sort -n "$archivo"
+else
+    echo "No se encontró el archivo."
+fi
