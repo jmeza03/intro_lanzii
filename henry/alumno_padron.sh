db9798dd8a8674c96506fa6d7a654b08d7c1df81
luishenry145-coder <luishenry145@gmail.com>
2026-08-31 19:40:36 -0300

    saco el if anidado que no sirve
    Updated the condition to check for student ID presence in the file.

--- henry/alumno_padron.sh
+++ henry/alumno_padron.sh
@@ -2,13 +2,8 @@ read -p "ingresa el padron del alumno:" padron
 
 archivo=~/$CREACION/salida/$FILENAME.txt
 
-if [ -f "$archivo" ]; then
+if [ -f "$archivo" ] && grep -p "$padron" "$archivo"; then
     grep "^$padron" "$archivo"
-
-    if [ $? -ne 0 ]; then
-        echo "no se encontró ningún alumno con el padrón $padron"
-    fi
-    
 else
     echo "no se encontró el archivo"
 fi
