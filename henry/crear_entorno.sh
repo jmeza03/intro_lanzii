7b07c13881b9f7b4253d25ebf42892744dd8cc4e
luishenry145-coder <luishenry145@gmail.com>
2026-08-31 18:20:11 -0300

    Create environment setup script
    Add script to create environment directories if they don't exist.

--- henry/crear_entorno.sh
+++ henry/crear_entorno.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+if [ -d "$CREACION" ]; then #-d ve si exite el directorio
+    echo "el entorno ya existe"
+else
+    mkdir -p "$CREACION/entrada"
+    mkdir -p "$CREACION/salida"
+    mkdir -p "$CREACION/procesado"
+
+    echo f"entorno creado correctamente en: $CREACION"
+fi
