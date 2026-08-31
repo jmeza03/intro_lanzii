4872c4715f00563623cbb1dbe7db4c40d3b6851d
luishenry145-coder <luishenry145@gmail.com>
2026-08-31 18:19:31 -0300

    Implement student search by padron in alumno_padron.sh
    Added functionality to search for a student by their padron in a specified file and handle cases where the file or student is not found.

--- henry/alumno_padron.sh
+++ henry/alumno_padron.sh
@@ -1,1 +1,14 @@
+read -p "Ingrese el padron del alumno:" padron
 
+archivo=~/$CREACION/salida/$FILENAME.txt
+
+if [ -f "$archivo" ]; then
+    grep "^$padron" "$archivo"
+
+    if [ $? -ne 0 ]; then
+        echo "No se encontró ningún alumno con el padrón $padron."
+    fi
+    
+else
+    echo "No se encontró el archivo."
+fi
